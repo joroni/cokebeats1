190084b61cb56e39b81e10f2684c0912a52569ac
kophonegap <niconi@coca-cola.com>
2016-08-23 02:50:16 +0800

    new

--- www/themes/coke-bootstrap/js/launch.js
+++ www/themes/coke-bootstrap/js/launch.js
@@ -183,3 +183,13 @@ require(['root/config'],function(Config){
 	}
 
 });
+
+// set to either landscape
+screen.lockOrientation('landscape');
+
+// allow user rotate
+screen.unlockOrientation();
+
+// access current orientation
+console.log('Orientation is ' + screen.orientation);
+
