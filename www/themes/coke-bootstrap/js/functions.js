55bd7250f639915c210f73258d4de700237440fe
kophonegap <niconi@coca-cola.com>
2016-08-23 08:43:17 +0800

    new styles

--- www/themes/coke-bootstrap/js/functions.js
+++ www/themes/coke-bootstrap/js/functions.js
@@ -42,7 +42,7 @@ define( [ 'jquery', 'core/theme-app', 'core/theme-tpl-tags', 'core/modules/stora
 		Storage.clear( 'scroll-pos' );
 		$refresh_button.removeClass( 'refreshing' );
 		if ( result.ok ) {
-			$( '#feedback' ).removeClass( 'error' ).html( '<div style="background: #00ff00 none repeat scroll 0% 0%; color: #333 ! important;" id="feedback">Taste the Feeling</div>' ).slideDown();
+			$( '#feedback' ).removeClass( 'error' ).html( '<i id="feedback" class="fa fa-check-circle" style="color: #00ff00; font-size: 2em; text-shadow: 0 0 1px #888;"></i> ' ).slideDown();
 		} else {
 			$( '#feedback' ).addClass( 'error' ).html( result.message ).slideDown();
 		}
