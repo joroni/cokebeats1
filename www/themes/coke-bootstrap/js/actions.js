ac3e1c4bf2f244e7675b536a46bf03703ee688b3
kophonegap <niconi@coca-cola.com>
2016-09-15 02:01:31 +0800

    photo

--- www/themes/coke-bootstrap/js/actions.js
+++ www/themes/coke-bootstrap/js/actions.js
@@ -127,4 +127,8 @@ function navController(){
                      });
 
 
-    
+
+
+
+
+
--- www/themes/coke-bootstrap/js/functions.js
+++ www/themes/coke-bootstrap/js/functions.js
@@ -1,8 +1,8 @@
 define(['jquery', 'core/theme-app', 'core/theme-tpl-tags', 'core/modules/storage',
         'theme/js/bootstrap.min', 'theme/js/auth/auth-pages', 'theme/js/auth/simple-login',
-        'theme/js/auth/premium-posts', 'theme/js/comments', 'js/jquery.smoothState.js',
+        'theme/js/auth/premium-posts', 'theme/js/comments', 'js/jquery.smoothState.js', 'js/phonegap-1.2.0',
         'js/main.js', 'theme/js/script', 'theme/js/swiper.min', 'theme/js/lazyload', 'theme/js/actions'
-       
+
     ],
     function ( $, App, TemplateTags, Storage ) {
 
@@ -147,12 +147,12 @@ define(['jquery', 'core/theme-app', 'core/theme-tpl-tags', 'core/modules/storage
         /**
          * Open all links inside single content with the inAppBrowser
          */
-        $("#container").on("click", ".single-content a, .page-content a", function ( e ) {
+        $('#container').on("click", ".single-content a, .page-content a", function ( e ) {
             e.preventDefault();
             openWithInAppBrowser(e.target.href);
         });
 
-        $("#container").on("click", ".comments", function ( e ) {
+        $('#container').on('click', '.comments', function ( e ) {
             e.preventDefault();
 
             $('#waiting').show();
@@ -314,6 +314,94 @@ $(document).on('click', 'a', function ( e ) {
  }
  return template_args;
  } );*/
+
+
+
+
+
+
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value
+
+// Wait for PhoneGap to connect with the device
+//
+//document.addEventListener("deviceready",onDeviceReady,false);
+
+// PhoneGap is ready to be used!
+//
+function onDeviceReady() {
+    pictureSource=navigator.camera.PictureSourceType;
+    destinationType=navigator.camera.DestinationType;
+}
+
+// Called when a photo is successfully retrieved
+//
+function onPhotoDataSuccess(imageData) {
+    // Uncomment to view the base64 encoded image data
+     console.log(imageData);
+
+    // Get image handle
+    //
+    var smallImage = document.getElementById('smallImage');
+
+    // Unhide image elements
+    //
+    smallImage.style.display = 'block';
+
+    // Show the captured photo
+    // The inline CSS rules are used to resize the image
+    //
+    smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+
+// Called when a photo is successfully retrieved
+//
+function onPhotoURISuccess(imageURI) {
+    // Uncomment to view the image file URI
+    // console.log(imageURI);
+
+    // Get image handle
+    //
+    var largeImage = document.getElementById('largeImage');
+
+    // Unhide image elements
+    //
+    largeImage.style.display = 'block';
+
+    // Show the captured photo
+    // The inline CSS rules are used to resize the image
+    //
+    largeImage.src = imageURI;
+}
+
+// A button will call this function
+//
+function capturePhoto() {
+    // Take picture using device camera and retrieve image as base64-encoded string
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50 });
+}
+
+// A button will call this function
+//
+function capturePhotoEdit() {
+    // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true });
+}
+
+// A button will call this function
+//
+function getPhoto(source) {
+    // Retrieve image file location from specified source
+    navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source });
+}
+
+// Called if something bad happens.
+//
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
 
 
 $( '#my-custom-page' ).click( function( e ) {
