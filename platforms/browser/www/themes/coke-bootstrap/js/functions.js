76344a0c53e2fe13ac4853bb426016e7be7564a6
kophonegap <niconi@coca-cola.com>
2016-10-04 09:32:31 +0800

    icons and splashscreen

--- platforms/browser/www/themes/coke-bootstrap/js/functions.js
+++ platforms/browser/www/themes/coke-bootstrap/js/functions.js
@@ -1,10 +1,26 @@
 define(['jquery', 'core/theme-app', 'core/theme-tpl-tags', 'core/modules/storage',
         'theme/js/bootstrap.min', 'theme/photoswipe/photoswipe',
-        'theme/photoswipe/photoswipe-ui-default', 'theme/photoswipe/photoswipe-support', 'theme/js/zoom', 'theme/js/transition', 'theme/js/auth/auth-pages', 'theme/js/auth/simple-login',
+        'theme/photoswipe/photoswipe-ui-default', 'theme/photoswipe/photoswipe-support',   'theme/fancy/fancybox/jquery.easing.1.3', 'theme/fancy/fancybox/jquery.fancybox-1.3.4', 'theme/js/transition', 'theme/js/auth/auth-pages', 'theme/js/auth/simple-login',
         'theme/js/auth/premium-posts', 'theme/js/comments', 'js/jquery.smoothState.js', 'theme/js/phonegap-1.2.0',
         'js/main.js', 'theme/js/script', 'theme/js/swiper.min', 'theme/js/lazyload', 'theme/js/actions', 'theme/js/camera',
         'theme/js/moment.min', 'theme/js/velocity.min', 'theme/js/jquery.fitvids',
-        'theme/fancy/fancybox/jquery.easing.1.3', 'theme/fancy/fancybox/jquery.fancybox-1.3.4'
+  /*  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+    <!-- Add mousewheel plugin (this is optional) -->
+<script type="text/javascript" src="/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+
+    <!-- Add fancyBox -->
+<link rel="stylesheet" href="/fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+    <script type="text/javascript" src="/fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+
+    <!-- Optionally add helpers - button, thumbnail and/or media -->
+<link rel="stylesheet" href="/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+    <script type="text/javascript" src="/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+    <script type="text/javascript" src="/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.6"></script>
+
+    <link rel="stylesheet" href="/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+    <script type="text/javascript" src="/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script> */
+
         /*,'theme/vendor/wti-like-post/wti_like_post',*/
 
 
